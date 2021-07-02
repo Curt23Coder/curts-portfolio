4d314dfcb5e1231958e36712e4fa28c413b8a29a
Curtis Myers <curt.myers@aol.com>
2021-07-02 15:10:30 -0400

    Created and designed my new unique portfolio in HTML, CSS, Bootstrap5 and Jquery

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+        $(".jesus-image").animate({
+         opacity: "1"
+        },9000);
+        $(".faith").animate({
+          opacity: "1"  
+        },9000);
+    });
